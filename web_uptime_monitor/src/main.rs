a27a7be39bda414ed69975cb87d7024f23f58275
Johannes990 <johannes.jyrgenson@gmail.com>
2024-02-10 23:28:15 +0200

    structure changes and new dependencies

--- web_uptime_monitor/src/main.rs
+++ web_uptime_monitor/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use askama::Template;
+use chrono::Timelike;
+use askama_axum::IntoResponse as AskamaIntoResponse;
+use axum::{
+    extract::{Form, Path, State},
+    http::StatusCode,
+    response::{IntoResponse as AxumIntoResponse, Redirect, Response},
+    routing::{get, post},
+    Router,
+};
+use chrono::{DateTime, Utc};
+use futures_util::StreamExt;
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use sqlx::PgPool;
+use tokio::time::{self, Duration};
+use validator::Validate;
+
+
+#[derive(Clone)]
+struct AppState {
+    db: PgPool,
+}
+
+impl AppState {
+    fn new(db: PgPool) -> Self {
+        Self {db}
+    }
+}
+
+#[shuttle_runtime::main]
+async fn main(
+    #[shuttle_shared_db::Postgres] db: PgPool
+) -> shuttle_axum::ShuttleAxum {
+    sqlx::migrate!().run(&db).await.expect("Migrations went wrong:(");
+
+    let state = AppState::new(db);
 }
